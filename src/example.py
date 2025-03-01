2fe6e3b826a9b87e4706ecea85fe416106fe9cff
wuxxx949 <timnaka123@gmail.com>
2025-03-01 17:56:59 -0500

    compare with lm quantile reg

--- src/example.py
+++ src/example.py
@@ -1,8 +1,8 @@
 import numpy as np
-from rfgap import RFGAP
+from sklearn.linear_model import QuantileRegressor
 
 from plot_utils import plot_centered_bounds
-from rf_quantile_regression import predit_bounds, weighted_quantile_rfgap
+from rf_quantile_regression import predit_bounds
 
 if __name__ == "__main__":
     import openml
@@ -10,10 +10,22 @@ if __name__ == "__main__":
     prediction_type = 'regression'
 
     # Convert to a Pandas DataFrame
-    x, y, _, _ = boston.get_data(target=boston.default_target_attribute)
+    X, y, _, _ = boston.get_data(target=boston.default_target_attribute)
+    X['RAD'] = X['RAD'].astype(int)
+    X['CHAS'] = X['CHAS'].astype(int)
 
     upper_bound, lower_bound, rf = predit_bounds(
-        x, y, coverage=0.95, n_estimators=500, max_depth=6
+        X, y, coverage=0.95, n_estimators=500, max_depth=6
         )
 
     plot_centered_bounds(y, upper_bound, lower_bound)
+    print(f'average length of the bounds rf: {np.mean(upper_bound - lower_bound)}')
+
+    # compare with the sklearn implementation
+    qr = QuantileRegressor(quantile=0.025, alpha=0)
+    lower_bound_lm = qr.fit(X, y).predict(X.to_numpy())
+    qr = QuantileRegressor(quantile=0.975, alpha=0)
+    upper_bound_lm = qr.fit(X, y).predict(X)
+    plot_centered_bounds(y, upper_bound_lm, lower_bound_lm)
+    print(f'average length of the bounds lm: {np.mean(upper_bound_lm - lower_bound_lm)}')
+
